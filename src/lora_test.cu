e7b64a23d354a9b6af18104e7730efdcb16216ab
Ethan Siegel <ewsiegel@mit.edu>
2024-11-26 00:49:45 -0500

    add padding + smaller problem sizes to test

--- src/lora_test.cu
+++ src/lora_test.cu
@@ -27,6 +27,10 @@ void initialize_random_fp16(float* array_fp32, __half* array_fp16, int size, flo
     }
 }
 
+int round16(int n) {
+    return ((n + 15) / 16) * 16;
+}
+
 // Struct to store benchmark results
 struct BenchmarkResults {
     char const* name;
@@ -35,18 +39,17 @@ struct BenchmarkResults {
     std::map<std::tuple<int32_t, int32_t, int32_t, int32_t>, double> speedup;
 };
 
-// General benchmark function
 template <typename Impl>
 BenchmarkResults benchmark(const std::vector<Dimensions>& dimensions_list, int trials) {
     BenchmarkResults results;
     results.name = Impl::name;
 
     for (const auto& dims : dimensions_list) {
+        // Allocate unpadded host arrays for W, B, A, x
         size_t size_W = dims.size_m * dims.size_d * sizeof(__half);
         size_t size_B = dims.size_m * dims.size_r * sizeof(__half);
         size_t size_A = dims.size_r * dims.size_d * sizeof(__half);
         size_t size_x = dims.size_d * dims.size_b * sizeof(__half);
-        size_t size_y = dims.size_m * dims.size_b * sizeof(__half);
 
         float *h_W_fp32 = (float*)malloc(dims.size_m * dims.size_d * sizeof(float));
         float *h_B_fp32 = (float*)malloc(dims.size_m * dims.size_r * sizeof(float));
@@ -57,32 +60,69 @@ BenchmarkResults benchmark(const std::vector<Dimensions>& dimensions_list, int t
         __half *h_B = (__half*)malloc(size_B);
         __half *h_A = (__half*)malloc(size_A);
         __half *h_x = (__half*)malloc(size_x);
-        __half *h_y = (__half*)malloc(size_y);
 
+        // Initialize random values
         unsigned int seed = 42;
         initialize_random_fp16(h_W_fp32, h_W, dims.size_m * dims.size_d, -1.0f, 1.0f, seed);
         initialize_random_fp16(h_B_fp32, h_B, dims.size_m * dims.size_r, -1.0f, 1.0f, seed);
         initialize_random_fp16(h_A_fp32, h_A, dims.size_r * dims.size_d, -1.0f, 1.0f, seed);
         initialize_random_fp16(h_x_fp32, h_x, dims.size_d * dims.size_b, -1.0f, 1.0f, seed);
 
+        // Pad dimensions to the nearest multiple of 16
+        int padded_m = round16(dims.size_m);
+        int padded_d = round16(dims.size_d);
+        int padded_b = round16(dims.size_b);
+        int padded_r = round16(dims.size_r);
+
+        // Calculate padded sizes
+        size_t size_W_padded = padded_m * padded_d * sizeof(__half);
+        size_t size_B_padded = padded_m * padded_r * sizeof(__half);
+        size_t size_A_padded = padded_r * padded_d * sizeof(__half);
+        size_t size_x_padded = padded_d * padded_b * sizeof(__half);
+        size_t size_y_padded = padded_m * padded_b * sizeof(__half);
+
+        // Allocate padded host arrays
+        __half *h_W_padded = (__half*)calloc(padded_m * padded_d, sizeof(__half));
+        __half *h_B_padded = (__half*)calloc(padded_m * padded_r, sizeof(__half));
+        __half *h_A_padded = (__half*)calloc(padded_r * padded_d, sizeof(__half));
+        __half *h_x_padded = (__half*)calloc(padded_d * padded_b, sizeof(__half));
+        __half *h_y_padded = (__half*)calloc(padded_m * padded_b, sizeof(__half));
+
+        // Copy and pad input matrices
+        for (int i = 0; i < dims.size_m; ++i)
+            for (int j = 0; j < dims.size_d; ++j)
+                h_W_padded[i * padded_d + j] = h_W[i * dims.size_d + j];
+        for (int i = 0; i < dims.size_m; ++i)
+            for (int j = 0; j < dims.size_r; ++j)
+                h_B_padded[i * padded_r + j] = h_B[i * dims.size_r + j];
+        for (int i = 0; i < dims.size_r; ++i)
+            for (int j = 0; j < dims.size_d; ++j)
+                h_A_padded[i * padded_d + j] = h_A[i * dims.size_d + j];
+        for (int i = 0; i < dims.size_d; ++i)
+            for (int j = 0; j < dims.size_b; ++j)
+                h_x_padded[i * padded_b + j] = h_x[i * dims.size_b + j];
+
+        // Allocate device memory
         __half *d_W, *d_B, *d_A, *d_x, *d_y, *d_tmp;
-        cudaMalloc(&d_W, size_W);
-        cudaMalloc(&d_B, size_B);
-        cudaMalloc(&d_A, size_A);
-        cudaMalloc(&d_x, size_x);
-        cudaMalloc(&d_y, size_y);
-        cudaMalloc(&d_tmp, Impl::get_workspace_size(dims.size_m, dims.size_d, dims.size_b, dims.size_r));
+        cudaMalloc(&d_W, size_W_padded);
+        cudaMalloc(&d_B, size_B_padded);
+        cudaMalloc(&d_A, size_A_padded);
+        cudaMalloc(&d_x, size_x_padded);
+        cudaMalloc(&d_y, size_y_padded);
+        cudaMalloc(&d_tmp, Impl::get_workspace_size(padded_m, padded_d, padded_b, padded_r));
 
-        cudaMemcpy(d_W, h_W, size_W, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_x, h_x, size_x, cudaMemcpyHostToDevice);
+        // Copy padded matrices to device
+        cudaMemcpy(d_W, h_W_padded, size_W_padded, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_B, h_B_padded, size_B_padded, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_A, h_A_padded, size_A_padded, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_x, h_x_padded, size_x_padded, cudaMemcpyHostToDevice);
 
+        // Run kernel and measure time
         double min_time_ms = 1e9;
 
         for (int i = 0; i < trials; ++i) {
             auto start = std::chrono::high_resolution_clock::now();
-            Impl::run(d_W, d_x, d_B, d_A, d_y, dims, d_tmp);  // Call the templated LoRA implementation
+            Impl::run(d_W, d_x, d_B, d_A, d_y, {padded_m, padded_d, padded_b, padded_r}, d_tmp);
             cudaDeviceSynchronize();
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double, std::milli> elapsed = end - start;
@@ -92,28 +132,40 @@ BenchmarkResults benchmark(const std::vector<Dimensions>& dimensions_list, int t
             }
         }
 
-        cudaMemcpy(h_y, d_y, size_y, cudaMemcpyDeviceToHost);
+        // Copy the result back to host
+        cudaMemcpy(h_y_padded, d_y, size_y_padded, cudaMemcpyDeviceToHost);
+
+        // Extract the valid results
+        __half *h_y_valid = (__half*)malloc(dims.size_m * dims.size_b * sizeof(__half));
+        for (int i = 0; i < dims.size_m; ++i)
+            for (int j = 0; j < dims.size_b; ++j)
+                h_y_valid[i * dims.size_b + j] = h_y_padded[i * padded_b + j];
 
         // Save results
         auto dim_tuple = std::make_tuple(dims.size_m, dims.size_d, dims.size_b, dims.size_r);
-        results.outputs[dim_tuple] = h_y;
+        results.outputs[dim_tuple] = h_y_valid;
         results.elapsed_ms[dim_tuple] = min_time_ms;
 
+        // Free device and host memory
         cudaFree(d_W);
         cudaFree(d_B);
         cudaFree(d_A);
         cudaFree(d_x);
         cudaFree(d_y);
         cudaFree(d_tmp);
-        free(h_W_fp32);
-        free(h_B_fp32);
-        free(h_A_fp32);
-        free(h_x_fp32);
         free(h_W);
         free(h_B);
         free(h_A);
         free(h_x);
-        // Do not free h_y as it's stored in results.outputs
+        free(h_W_fp32);
+        free(h_B_fp32);
+        free(h_A_fp32);
+        free(h_x_fp32);
+        free(h_W_padded);
+        free(h_B_padded);
+        free(h_A_padded);
+        free(h_x_padded);
+        free(h_y_padded);
     }
 
     return results;
@@ -121,150 +173,55 @@ BenchmarkResults benchmark(const std::vector<Dimensions>& dimensions_list, int t
 
 int main() {
     std::vector<Dimensions> dimensions_list = {
-        {2048, 2048, 16, 16},
-        {2048, 2048, 16, 32},
-        {2048, 2048, 16, 64},
-        {2048, 2048, 16, 128},
-        {2048, 2048, 32, 16},
-        {2048, 2048, 32, 32},
-        {2048, 2048, 32, 64},
-        {2048, 2048, 32, 128},
-        {2048, 2048, 64, 16},
-        {2048, 2048, 64, 32},
-        {2048, 2048, 64, 64},
-        {2048, 2048, 64, 128},
-        {2048, 2048, 128, 16},
-        {2048, 2048, 128, 32},
-        {2048, 2048, 128, 64},
-        {2048, 2048, 128, 128},
-        //{2048, 2048, 256, 16},
-        //{2048, 2048, 256, 32},
-        //{2048, 2048, 256, 64},
-        //{2048, 2048, 256, 128},
-        //{2048, 2048, 512, 16},
-        //{2048, 2048, 512, 32},
-        //{2048, 2048, 512, 64},
-        //{2048, 2048, 512, 128},
-        //{2048, 2048, 1024, 16},
-        //{2048, 2048, 1024, 32},
-        //{2048, 2048, 1024, 64},
-        //{2048, 2048, 1024, 128},
+        // {2048, 2048, ...}
+        {2048, 2048, 1, 1}, {2048, 2048, 1, 2}, {2048, 2048, 1, 4}, {2048, 2048, 1, 8}, {2048, 2048, 1, 16}, {2048, 2048, 1, 32}, {2048, 2048, 1, 64},
+        {2048, 2048, 2, 1}, {2048, 2048, 2, 2}, {2048, 2048, 2, 4}, {2048, 2048, 2, 8}, {2048, 2048, 2, 16}, {2048, 2048, 2, 32}, {2048, 2048, 2, 64},
+        {2048, 2048, 4, 1}, {2048, 2048, 4, 2}, {2048, 2048, 4, 4}, {2048, 2048, 4, 8}, {2048, 2048, 4, 16}, {2048, 2048, 4, 32}, {2048, 2048, 4, 64},
+        {2048, 2048, 8, 1}, {2048, 2048, 8, 2}, {2048, 2048, 8, 4}, {2048, 2048, 8, 8}, {2048, 2048, 8, 16}, {2048, 2048, 8, 32}, {2048, 2048, 8, 64},
+        {2048, 2048, 16, 1}, {2048, 2048, 16, 2}, {2048, 2048, 16, 4}, {2048, 2048, 16, 8}, {2048, 2048, 16, 16}, {2048, 2048, 16, 32}, {2048, 2048, 16, 64},
+        {2048, 2048, 32, 1}, {2048, 2048, 32, 2}, {2048, 2048, 32, 4}, {2048, 2048, 32, 8}, {2048, 2048, 32, 16}, {2048, 2048, 32, 32}, {2048, 2048, 32, 64},
+        {2048, 2048, 64, 1}, {2048, 2048, 64, 2}, {2048, 2048, 64, 4}, {2048, 2048, 64, 8}, {2048, 2048, 64, 16}, {2048, 2048, 64, 32}, {2048, 2048, 64, 64},
+        {2048, 2048, 128, 1}, {2048, 2048, 128, 2}, {2048, 2048, 128, 4}, {2048, 2048, 128, 8}, {2048, 2048, 128, 16}, {2048, 2048, 128, 32}, {2048, 2048, 128, 64},
 
-        {1024, 1024, 16, 16},
-        {1024, 1024, 16, 32},
-        {1024, 1024, 16, 64},
-        {1024, 1024, 16, 128},
-        {1024, 1024, 32, 16},
-        {1024, 1024, 32, 32},
-        {1024, 1024, 32, 64},
-        {1024, 1024, 32, 128},
-        {1024, 1024, 64, 16},
-        {1024, 1024, 64, 32},
-        {1024, 1024, 64, 64},
-        {1024, 1024, 64, 128},
-        {1024, 1024, 128, 16},
-        {1024, 1024, 128, 32},
-        {1024, 1024, 128, 64},
-        {1024, 1024, 128, 128},
-        //{1024, 1024, 256, 16},
-        //{1024, 1024, 256, 32},
-        //{1024, 1024, 256, 64},
-        //{1024, 1024, 256, 128},
-        //{1024, 1024, 512, 16},
-        //{1024, 1024, 512, 32},
-        //{1024, 1024, 512, 64},
-        //{1024, 1024, 512, 128},
-        //{1024, 1024, 1024, 16},
-        //{1024, 1024, 1024, 32},
-        //{1024, 1024, 1024, 64},
-        //{1024, 1024, 1024, 128},
+        // {1024, 1024, ...}
+        {1024, 1024, 1, 1}, {1024, 1024, 1, 2}, {1024, 1024, 1, 4}, {1024, 1024, 1, 8}, {1024, 1024, 1, 16}, {1024, 1024, 1, 32}, {1024, 1024, 1, 64},
+        {1024, 1024, 2, 1}, {1024, 1024, 2, 2}, {1024, 1024, 2, 4}, {1024, 1024, 2, 8}, {1024, 1024, 2, 16}, {1024, 1024, 2, 32}, {1024, 1024, 2, 64},
+        {1024, 1024, 4, 1}, {1024, 1024, 4, 2}, {1024, 1024, 4, 4}, {1024, 1024, 4, 8}, {1024, 1024, 4, 16}, {1024, 1024, 4, 32}, {1024, 1024, 4, 64},
+        {1024, 1024, 8, 1}, {1024, 1024, 8, 2}, {1024, 1024, 8, 4}, {1024, 1024, 8, 8}, {1024, 1024, 8, 16}, {1024, 1024, 8, 32}, {1024, 1024, 8, 64},
+        {1024, 1024, 16, 1}, {1024, 1024, 16, 2}, {1024, 1024, 16, 4}, {1024, 1024, 16, 8}, {1024, 1024, 16, 16}, {1024, 1024, 16, 32}, {1024, 1024, 16, 64},
+        {1024, 1024, 32, 1}, {1024, 1024, 32, 2}, {1024, 1024, 32, 4}, {1024, 1024, 32, 8}, {1024, 1024, 32, 16}, {1024, 1024, 32, 32}, {1024, 1024, 32, 64},
+        {1024, 1024, 64, 1}, {1024, 1024, 64, 2}, {1024, 1024, 64, 4}, {1024, 1024, 64, 8}, {1024, 1024, 64, 16}, {1024, 1024, 64, 32}, {1024, 1024, 64, 64},
+        {1024, 1024, 128, 1}, {1024, 1024, 128, 2}, {1024, 1024, 128, 4}, {1024, 1024, 128, 8}, {1024, 1024, 128, 16}, {1024, 1024, 128, 32}, {1024, 1024, 128, 64},
 
-        {512, 512, 16, 16},
-        {512, 512, 16, 32},
-        {512, 512, 16, 64},
-        {512, 512, 16, 128},
-        {512, 512, 32, 16},
-        {512, 512, 32, 32},
-        {512, 512, 32, 64},
-        {512, 512, 32, 128},
-        {512, 512, 64, 16},
-        {512, 512, 64, 32},
-        {512, 512, 64, 64},
-        {512, 512, 64, 128},
-        {512, 512, 128, 16},
-        {512, 512, 128, 32},
-        {512, 512, 128, 64},
-        {512, 512, 128, 128},
-        //{512, 512, 256, 16},
-        //{512, 512, 256, 32},
-        //{512, 512, 256, 64},
-        //{512, 512, 256, 128},
-        //{512, 512, 512, 16},
-        //{512, 512, 512, 32},
-        //{512, 512, 512, 64},
-        //{512, 512, 512, 128},
-        //{512, 512, 1024, 16},
-        //{512, 512, 1024, 32},
-        //{512, 512, 1024, 64},
-        //{512, 512, 1024, 128},
+        // {512, 512, ...}
+        {512, 512, 1, 1}, {512, 512, 1, 2}, {512, 512, 1, 4}, {512, 512, 1, 8}, {512, 512, 1, 16}, {512, 512, 1, 32}, {512, 512, 1, 64},
+        {512, 512, 2, 1}, {512, 512, 2, 2}, {512, 512, 2, 4}, {512, 512, 2, 8}, {512, 512, 2, 16}, {512, 512, 2, 32}, {512, 512, 2, 64},
+        {512, 512, 4, 1}, {512, 512, 4, 2}, {512, 512, 4, 4}, {512, 512, 4, 8}, {512, 512, 4, 16}, {512, 512, 4, 32}, {512, 512, 4, 64},
+        {512, 512, 8, 1}, {512, 512, 8, 2}, {512, 512, 8, 4}, {512, 512, 8, 8}, {512, 512, 8, 16}, {512, 512, 8, 32}, {512, 512, 8, 64},
+        {512, 512, 16, 1}, {512, 512, 16, 2}, {512, 512, 16, 4}, {512, 512, 16, 8}, {512, 512, 16, 16}, {512, 512, 16, 32}, {512, 512, 16, 64},
+        {512, 512, 32, 1}, {512, 512, 32, 2}, {512, 512, 32, 4}, {512, 512, 32, 8}, {512, 512, 32, 16}, {512, 512, 32, 32}, {512, 512, 32, 64},
+        {512, 512, 64, 1}, {512, 512, 64, 2}, {512, 512, 64, 4}, {512, 512, 64, 8}, {512, 512, 64, 16}, {512, 512, 64, 32}, {512, 512, 64, 64},
+        {512, 512, 128, 1}, {512, 512, 128, 2}, {512, 512, 128, 4}, {512, 512, 128, 8}, {512, 512, 128, 16}, {512, 512, 128, 32}, {512, 512, 128, 64},
 
-        {256, 256, 16, 16},
-        {256, 256, 16, 32},
-        {256, 256, 16, 64},
-        {256, 256, 16, 128},
-        {256, 256, 32, 16},
-        {256, 256, 32, 32},
-        {256, 256, 32, 64},
-        {256, 256, 32, 128},
-        {256, 256, 64, 16},
-        {256, 256, 64, 32},
-        {256, 256, 64, 64},
-        {256, 256, 64, 128},
-        {256, 256, 128, 16},
-        {256, 256, 128, 32},
-        {256, 256, 128, 64},
-        {256, 256, 128, 128},
-        //{256, 256, 256, 16},
-        //{256, 256, 256, 32},
-        //{256, 256, 256, 64},
-        //{256, 256, 256, 128},
-        //{256, 256, 512, 16},
-        //{256, 256, 512, 32},
-        //{256, 256, 512, 64},
-        //{256, 256, 512, 128},
-        //{256, 256, 1024, 16},
-        //{256, 256, 1024, 32},
-        //{256, 256, 1024, 64},
-        //{256, 256, 1024, 128},
+        // {256, 256, ...}
+        {256, 256, 1, 1}, {256, 256, 1, 2}, {256, 256, 1, 4}, {256, 256, 1, 8}, {256, 256, 1, 16}, {256, 256, 1, 32}, {256, 256, 1, 64},
+        {256, 256, 2, 1}, {256, 256, 2, 2}, {256, 256, 2, 4}, {256, 256, 2, 8}, {256, 256, 2, 16}, {256, 256, 2, 32}, {256, 256, 2, 64},
+        {256, 256, 4, 1}, {256, 256, 4, 2}, {256, 256, 4, 4}, {256, 256, 4, 8}, {256, 256, 4, 16}, {256, 256, 4, 32}, {256, 256, 4, 64},
+        {256, 256, 8, 1}, {256, 256, 8, 2}, {256, 256, 8, 4}, {256, 256, 8, 8}, {256, 256, 8, 16}, {256, 256, 8, 32}, {256, 256, 8, 64},
+        {256, 256, 16, 1}, {256, 256, 16, 2}, {256, 256, 16, 4}, {256, 256, 16, 8}, {256, 256, 16, 16}, {256, 256, 16, 32}, {256, 256, 16, 64},
+        {256, 256, 32, 1}, {256, 256, 32, 2}, {256, 256, 32, 4}, {256, 256, 32, 8}, {256, 256, 32, 16}, {256, 256, 32, 32}, {256, 256, 32, 64},
+        {256, 256, 64, 1}, {256, 256, 64, 2}, {256, 256, 64, 4}, {256, 256, 64, 8}, {256, 256, 64, 16}, {256, 256, 64, 32}, {256, 256, 64, 64},
+        {256, 256, 128, 1}, {256, 256, 128, 2}, {256, 256, 128, 4}, {256, 256, 128, 8}, {256, 256, 128, 16}, {256, 256, 128, 32}, {256, 256, 128, 64},
 
-        {128, 128, 16, 16},
-        {128, 128, 16, 32},
-        {128, 128, 16, 64},
-        {128, 128, 16, 128},
-        {128, 128, 32, 16},
-        {128, 128, 32, 32},
-        {128, 128, 32, 64},
-        {128, 128, 32, 128},
-        {128, 128, 64, 16},
-        {128, 128, 64, 32},
-        {128, 128, 64, 64},
-        {128, 128, 64, 128},
-        {128, 128, 128, 16},
-        {128, 128, 128, 32},
-        {128, 128, 128, 64},
-        {128, 128, 128, 128},
-        //{128, 128, 256, 16},
-        //{128, 128, 256, 32},
-        //{128, 128, 256, 64},
-        //{128, 128, 256, 128},
-        //{128, 128, 512, 16},
-        //{128, 128, 512, 32},
-        //{128, 128, 512, 64},
-        //{128, 128, 512, 128},
-        //{128, 128, 1024, 16},
-        //{128, 128, 1024, 32},
-        //{128, 128, 1024, 64},
-        //{128, 128, 1024, 128},
+        // {128, 128, ...}
+        {128, 128, 1, 1}, {128, 128, 1, 2}, {128, 128, 1, 4}, {128, 128, 1, 8}, {128, 128, 1, 16}, {128, 128, 1, 32}, {128, 128, 1, 64},
+        {128, 128, 2, 1}, {128, 128, 2, 2}, {128, 128, 2, 4}, {128, 128, 2, 8}, {128, 128, 2, 16}, {128, 128, 2, 32}, {128, 128, 2, 64},
+        {128, 128, 4, 1}, {128, 128, 4, 2}, {128, 128, 4, 4}, {128, 128, 4, 8}, {128, 128, 4, 16}, {128, 128, 4, 32}, {128, 128, 4, 64},
+        {128, 128, 8, 1}, {128, 128, 8, 2}, {128, 128, 8, 4}, {128, 128, 8, 8}, {128, 128, 8, 16}, {128, 128, 8, 32}, {128, 128, 8, 64},
+        {128, 128, 16, 1}, {128, 128, 16, 2}, {128, 128, 16, 4}, {128, 128, 16, 8}, {128, 128, 16, 16}, {128, 128, 16, 32}, {128, 128, 16, 64},
+        {128, 128, 32, 1}, {128, 128, 32, 2}, {128, 128, 32, 4}, {128, 128, 32, 8}, {128, 128, 32, 16}, {128, 128, 32, 32}, {128, 128, 32, 64},
+        {128, 128, 64, 1}, {128, 128, 64, 2}, {128, 128, 64, 4}, {128, 128, 64, 8}, {128, 128, 64, 16}, {128, 128, 64, 32}, {128, 128, 64, 64},
+        {128, 128, 128, 1}, {128, 128, 128, 2}, {128, 128, 128, 4}, {128, 128, 128, 8}, {128, 128, 128, 16}, {128, 128, 128, 32}, {128, 128, 128, 64},
     };
     std::vector<BenchmarkResults> results;
 
